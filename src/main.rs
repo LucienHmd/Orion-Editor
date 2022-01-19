eca8c4b763bdeff58a2a9398267977ae32040f28
LucienHmd <llhammond9@gmail.com>
2022-01-19 15:47:52 -0500

    Changed backend from Crossterm to Termion

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::io;
+use termion::raw::IntoRawMode;
 use tui::Terminal;
-use tui::backend::CrosstermBackend;
+use tui::backend::TermionBackend;
 use tui::widgets::{Widget, Block, Borders};
 use tui::layout::{Layout, Constraint, Direction};
 
-
 fn main() -> Result<(), io::Error> {
-    let stdout = io::stdout();
-    let backend = CrosstermBackend::new(stdout);
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     terminal.draw(|f| {
         let size = f.size();
